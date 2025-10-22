5e09bf062c7de63e26c41a536390cfc4d19ebaf9
sean <sbelomet@student.42lausanne.ch>
2025-10-22 16:18:51 +0200

    starting on camera

--- src/camera_utils.c
+++ src/camera_utils.c
@@ -6,7 +6,7 @@
 /*   By: sbelomet <sbelomet@42lausanne.ch>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/22 13:23:01 by sbelomet          #+#    #+#             */
-/*   Updated: 2025/10/22 13:09:49 by sbelomet         ###   ########.fr       */
+/*   Updated: 2025/10/22 16:16:54 by sbelomet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,4 +15,8 @@
 t_mat4	ft_lookat(const t_vec3 camPos, const t_vec3 targetPos, const t_vec3 worldUp)
 {
 	t_vec3	cam_dir = ft_vec3_normalize(ft_vec3_sub(camPos, targetPos));
+	t_vec3	cam_right = ft_vec3_normalize(ft_vec3_cross(worldUp, cam_dir));
+	t_vec3	cam_up = ft_vec3_cross(cam_dir, cam_right);
+
+	
 }
